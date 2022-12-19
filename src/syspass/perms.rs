476e04cb24f2d2afaf828ecc0c0626cfd680ba71
Eugene Lebedev <eugene.0x90@gmail.com>
2022-12-19 20:49:19 +0300

    Set permissions for account

--- src/syspass/perms.rs
+++ src/syspass/perms.rs
@@ -1,8 +1,8 @@
 use std::thread;
 use std::time::Duration;
 
-use log::{debug, info};
-use thirtyfour::{By, Key, WebDriver};
+use log::{debug, error, info};
+use thirtyfour::{By, Key, WebDriver, WebElement};
 
 use crate::config::PermissionsConfig;
 use crate::types::EmptyResult;
@@ -16,7 +16,6 @@ pub async fn set_permissions_for_account_in_syspass(
     let url = format!("{}/index.php?r=index", syspass_base_url);
 
     driver.goto(&url).await?;
-    driver.refresh().await?;
 
     thread::sleep(Duration::new(2, 0));
 
@@ -53,7 +52,79 @@ pub async fn set_permissions_for_account_in_syspass(
 
             edit_item.click().await?;
 
+            thread::sleep(Duration::new(1, 0));
+
+            let tabs = driver.find_all(By::ClassName("mdl-tabs__tab")).await?;
+
+            if tabs.len() == 3 {
+                let perms_tab = tabs.get(1).unwrap();
+                perms_tab.click().await?;
+
+                let click_for_close_element = driver.find(By::Id("frmAccount")).await?;
+
+                let perm_inputs = driver.find_all(By::ClassName("tag-list-box")).await?;
+
+                if perm_inputs.len() == 4 {
+                    info!("add user view permissions");
+                    set_permissions_for_security_entity(&perm_inputs, 0,
+                                                        &user_permissions.view).await?;
+
+                    click_for_close_element.click().await?;
+
+                    info!("add user edit permissions");
+                    set_permissions_for_security_entity(&perm_inputs, 1,
+                                                        &user_permissions.edit).await?;
+
+                    click_for_close_element.click().await?;
+
+                    info!("add group view permissions");
+                    set_permissions_for_security_entity(&perm_inputs, 2,
+                                                        &group_permissions.view).await?;
+
+                    click_for_close_element.click().await?;
+
+                    info!("add group edit permissions");
+                    set_permissions_for_security_entity(&perm_inputs, 3,
+                                                        &group_permissions.edit).await?;
+
+                    click_for_close_element.click().await?;
+
+                } else {
+                    error!("unsupported syspass ui version, 4 divs expected with class 'tag-list-box'")
+                }
+
+            } else {
+                error!("unsupported syspass ui version, 3 tabs expected")
+            }
+
             break;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn set_permissions_for_security_entity(perm_inputs: &Vec<WebElement>,
+                                                 perm_input_index: usize,
+                                                 permissions: &Vec<String>) -> EmptyResult {
+    debug!("set permissions for security entity: {:?}", permissions);
+
+    if !permissions.is_empty() {
+        let perms_input = perm_inputs.get(perm_input_index).unwrap();
+        perms_input.click().await?;
+
+        for permission in permissions {
+            info!("- add '{}'", permission);
+            let options = perms_input.find_all(By::ClassName("option")).await?;
+
+            for option in options {
+                let text = option.text().await?;
+
+                if &text == permission {
+                    info!("- add '{}' - success", permission);
+                    option.click().await?;
+                }
+            }
         }
     }
 
