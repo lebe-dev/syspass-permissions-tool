e4406f606dd67ad40483637f1e8d2d729bec4ab0
Eugene Lebedev <eugene.0x90@gmail.com>
2022-12-20 11:51:29 +0300

    Refactoring

--- src/feature/perms.rs
+++ src/feature/perms.rs
@@ -18,74 +18,67 @@ pub async fn set_permissions_for_accounts_in_syspass(config: &AppConfig,
 
     let xml_config = get_xml_config_from_file(xml_file)?;
 
-    match login_to_syspass(&driver, &config.syspass_url,
-                           &config.auth.login, &config.auth.password).await {
-        Ok(_) => {
-            info!("user '{}' logged to syspass", &config.auth.login);
+    login_to_syspass(&driver, &config.syspass_url,
+                     &config.auth.login, &config.auth.password).await?;
 
-            let mut has_errors = false;
+    info!("user '{}' logged to syspass", &config.auth.login);
 
-            for account in xml_config.accounts {
-                let client_found = xml_config.clients.iter()
-                    .find(|client|client.id == account.client_id);
+    let mut has_errors = false;
 
-                match client_found {
-                    Some(client) => {
+    for account in xml_config.accounts {
+        let client_found = xml_config.clients.iter()
+            .find(|client|client.id == account.client_id);
 
-                        let category_found = xml_config.categories.iter().find(|category| category.id == account.category_id);
+        match client_found {
+            Some(client) => {
 
-                        match category_found {
-                            Some(category) => {
-                                match set_permissions_for_account(
-                                    &driver, &config.syspass_url,
-                                    &account.login, &client.name,
-                                    &category.name, &config.permissions
-                                ).await {
-                                    Ok(_) => info!("permissions have been set for account login '{}'", account.login),
-                                    Err(e) => {
-                                        error!("{}", e);
-                                        error!("couldn't find account '{}'", account.login);
-                                        has_errors = true;
-                                    },
-                                }
-                            }
-                            None => {
-                                error!("account configuration error, client wasn't found by id {}", account.category_id);
+                let category_found = xml_config.categories.iter().find(|category| category.id == account.category_id);
+
+                match category_found {
+                    Some(category) => {
+                        match set_permissions_for_account(
+                            &driver, &config.syspass_url,
+                            &account.login, &client.name,
+                            &category.name, &config.permissions
+                        ).await {
+                            Ok(_) => info!("permissions have been set for account login '{}'", account.login),
+                            Err(e) => {
+                                error!("{}", e);
+                                error!("couldn't find account '{}'", account.login);
                                 has_errors = true;
-                            }
+                            },
                         }
-
                     }
                     None => {
-                        error!("account configuration error, client wasn't found by id {}", account.client_id);
+                        error!("account configuration error, client wasn't found by id {}", account.category_id);
                         has_errors = true;
                     }
                 }
 
-                if has_errors && !config.ignore_errors {
-                    info!("process has been interrupted due error")
-                }
-
             }
-
-            if !has_errors {
-                info!("permissions have been set for accounts");
-                Ok(())
-
-            } else {
-                if !config.ignore_errors {
-                    Err(anyhow!("process has been interrupted due error"))
-
-                } else {
-                    info!("permissions have been partially set for accounts");
-                    Ok(())
-                }
+            None => {
+                error!("account configuration error, client wasn't found by id {}", account.client_id);
+                has_errors = true;
             }
-
         }
-        Err(e) => {
-            error!("syspass auth error: {}", e);
-            Err(anyhow!("syspass auth error"))
+
+        if has_errors && !config.ignore_errors {
+            info!("process has been interrupted due error")
+        }
+
+    }
+
+    if !has_errors {
+        info!("permissions have been set for accounts");
+        Ok(())
+
+    } else {
+        if !config.ignore_errors {
+            Err(anyhow!("process has been interrupted due error"))
+
+        } else {
+            info!("permissions have been partially set for accounts");
+            Ok(())
         }
     }
 
